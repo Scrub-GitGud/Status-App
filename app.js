cb5e7b245a981bd3e7f47e96b1b87fa70b6c71e2
rifatNR <rifatnoor92@gmail.com>
2020-05-18 22:29:11 +0600

    Add new discussion system with modal form and clickable discussion to go to there respective page.

--- app.js
+++ app.js
@@ -24,12 +24,28 @@ mongoose.connect('mongodb://localhost:27017/STATUS_APP_DB', {useNewUrlParser: tr
 mongoose.set('useCreateIndex', true); // TO AVOID DeprecationWarning
 
 const userSchema = new mongoose.Schema({
-    Username: String,
-    Password: String
+    username: String,
+    password: String
 })
+const postSchema = {
+    post_creator: String,
+    post_content: String,
+    liker: String,
+    comment: String,
+    commenter: String
+}
+const discussionSchema = {
+    discussion_title: String,
+    details: String,
+    Posts: [postSchema],
+}
+
 
 userSchema.plugin(passportLocalMongoose);
+
 const UserCollection = new mongoose.model("UserCollection", userSchema)
+const postCollection = new mongoose.model("postCollection", postSchema)
+const discussionCollection = new mongoose.model("discussionCollection", discussionSchema)
 
 passport.use(UserCollection.createStrategy());
 passport.serializeUser(UserCollection.serializeUser());
@@ -43,7 +59,11 @@ let GLOBAL_USERNAME;
 app.route("/")
 .get((req, res)=>{
     if(req.isAuthenticated()){ // IF THE USER ALREADY LOGGED IN THEN RENDER HOME, ELSE RENDER LOGIN
-        res.render("home", {name: GLOBAL_USERNAME})
+        // res.render("home", {name: GLOBAL_USERNAME})
+        discussionCollection.find({}, (err, results)=>{
+            if(err) console.log(err);
+            res.render("home", {name: GLOBAL_USERNAME, allDiscussions: results})
+        })
     }else{
         console.log("NEED AUTHENTICATON BEFORE ENTERING HOME");
         res.redirect("/login")
@@ -99,6 +119,52 @@ app.route("/logout")
     req.logout()
     res.redirect("/login")
 });
+// ============================================== Authentication ==================================
+
+    
+
+
+// ============================================== Discussion ==================================
+app.post("/newDiscussion", (req, res)=>
+{
+    const newTitle = req.body.newDiscussionTitle
+    const newDetail = req.body.newDiscussionDetail
+    console.log("===> " + newTitle);
+    console.log("===> " + newDetail);
+
+      
+    let newDiscussion = new discussionCollection({
+        discussion_title: newTitle,
+        details: newDetail
+    })
+
+    newDiscussion.save();
+
+    res.redirect("/")
+})
+
+app.get("/discussion/:discussionID", (req, res)=>{
+    const requestedDiscussion = req.params.discussionID
+    // const requestedDiscussion_lodash = _.lowerCase(requestedDiscussion);
+
+    console.log("requestedDiscussion    => " + requestedDiscussion);
+    // console.log("lodashed version => " + requestedDiscussion_lodash);
+    
+    discussionCollection.findOne({discussion_title : requestedDiscussion}, (err, result)=>{
+      res.render("discussion" , {DiscussionTitle: result.discussion_title, DiscussionDetail: result.details})
+    })
+  
+    // postAry.forEach(i => {
+    //   if(_.lowerCase(i.title) === requestedPost){
+    //     res.render("post", {XpostTitle: i.title, XpostContent: i.content})
+    //   }
+    // });
+  })
+
+
+
+
+
 
 
 
